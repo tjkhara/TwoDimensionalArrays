d3a642b7991c4a7438f060667cf380d6650564a3
Tajeshwar Khara <tkhara@gmail.com>
2016-10-20 15:51:03 +0530

    Added the diagonal elements function
    Added function to display diagonal elements

--- TwoDimensionalArrays/Program.cs
+++ TwoDimensionalArrays/Program.cs
@@ -10,21 +10,42 @@ namespace TwoDimensionalArrays
     {
         static void Main(string[] args)
         {
-            CreateAndDisplayTwoDimensionalArray();
+            //CreateAndDisplayTwoDimensionalArray();
+            DisplayDiagonalElements();
         }
 
-        static void CreateAndDisplayTwoDimensionalArray()
+        //The function below displays a 3 by 3 two dimensional array
+
+        //static void CreateAndDisplayTwoDimensionalArray()
+        //{
+        //    int[,] array = new int[3, 3] { { 12, 11, 13 }, { 14, 1, 15 }, { 45, 75, 5 } };
+            
+        //    for(int i = 0; i < 3; i++)
+        //    {
+        //        for(int j = 0; j < 3; j++)
+        //        {
+        //            Console.Write(array[i, j] + " ");
+        //        }
+
+        //        Console.WriteLine();
+        //    }
+        //}
+
+        // The function below displays the diagonal elements of a 3 by 3 two dimensional array
+
+        static void DisplayDiagonalElements()
         {
             int[,] array = new int[3, 3] { { 12, 11, 13 }, { 14, 1, 15 }, { 45, 75, 5 } };
-            
-            for(int i = 0; i < 3; i++)
-            {
-                for(int j = 0; j < 3; j++)
-                {
-                    Console.Write(array[i, j] + " ");
-                }
 
-                Console.WriteLine();
+            for (int i = 0; i < 3; i++)
+            {
+                for (int j = 0; j < 3; j++)
+                {
+                    if (i == j)
+                    {
+                        Console.Write(array[i, j] + " ");
+                    } 
+                }
             }
         }
     }
