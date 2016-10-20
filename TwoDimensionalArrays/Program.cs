cf8e97cf4c114052a9807331b62c31fb5bcc057f
Tajeshwar Khara <tkhara@gmail.com>
2016-10-20 15:45:20 +0530

    Two Dimensional Arrays
    Creating and displaying a two dimensional array

---
+++ TwoDimensionalArrays/Program.cs
@@ -0,0 +1,31 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace TwoDimensionalArrays
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            CreateAndDisplayTwoDimensionalArray();
+        }
+
+        static void CreateAndDisplayTwoDimensionalArray()
+        {
+            int[,] array = new int[3, 3] { { 12, 11, 13 }, { 14, 1, 15 }, { 45, 75, 5 } };
+            
+            for(int i = 0; i < 3; i++)
+            {
+                for(int j = 0; j < 3; j++)
+                {
+                    Console.Write(array[i, j] + " ");
+                }
+
+                Console.WriteLine();
+            }
+        }
+    }
+}
